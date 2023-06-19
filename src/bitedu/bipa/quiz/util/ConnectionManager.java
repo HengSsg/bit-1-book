80eea6a0f1bd962f7a027e560d87aae4f1a3ec13
HyungSeok Park <dnflekf2748@gmail.com>
2023-06-19 14:10:08 +0900

    properties 기능 추가

--- src/bitedu/bipa/quiz/util/ConnectionManager.java
+++ src/bitedu/bipa/quiz/util/ConnectionManager.java
@@ -1,54 +1,57 @@
 package bitedu.bipa.quiz.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class ConnectionManager {
-	
-	public static Connection getConnection() {
-		Connection con = null;
-		String jdbcURL = "jdbc:mysql://localhost:3306/bitedu";
-		String driver = "com.mysql.cj.jdbc.Driver";
-		String id = "root";
-		String pwd = "gkgkgk12";
-		
-		try {
-			Class.forName(driver);
-			con = DriverManager.getConnection(jdbcURL,id,pwd);
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+    public static Connection getConnection() {
+        Connection con = null;
 
-		return con;
-	}
-	
-	public static void closeConnection(ResultSet rs, Statement stmt, Connection con) {
-		if(rs!=null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		if(stmt!=null) {
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		if(con!=null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+        Properties prop = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream("data/db.properties")) {
+            prop.load(fileInputStream);
+            String jdbcURL = prop.getProperty("jdbcURL");
+            String driver = prop.getProperty("driver");
+            String id = prop.getProperty("id");
+            String pwd = prop.getProperty("pwd");
+            Class.forName(driver);
+            con = DriverManager.getConnection(jdbcURL, id, pwd);
+
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void closeConnection(ResultSet rs, Statement stmt, Connection con) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
